a41a9838218bdc2ad9b50774833a670109746c60
vherzog <vherzog@bu.edu>
2015-12-11 21:03:39 -0500

    last commit

---
+++ app/src/main/java/ek327/finalproject/information.java
@@ -0,0 +1,34 @@
+package ek327.finalproject;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+        /**URL url = new URL(ek327app.comlu.com);
+         String data  = URLEncoder.encode("event_name", "UTF-8")
+         + "=" + URLEncoder.encode(event_name, "UTF-8");
+         data += "&" + URLEncoder.encode("ID", "UTF-8")
+         + "=" + URLEncoder.encode(ID, "UTF-8");
+         data += "&" + URLEncoder.encode("time", "UTF-8")
+         + "=" + URLEncoder.encode(time, "UTF-8");
+         data += "&" + URLEncoder.encode("location", "UTF-8")
+         + "=" + URLEncoder.encode(location, "UTF-8");
+         data += "&" + URLEncoder.encode("description", "UTF-8")
+         + "=" + URLEncoder.encode(description, "UTF-8");
+         data += "&" + URLEncoder.encode("organizer", "UTF-8")
+         + "=" + URLEncoder.encode(organizer, "UTF-8");
+         URLConnection conn = url.openConnection();
+         */
+
+    }
+
+}
